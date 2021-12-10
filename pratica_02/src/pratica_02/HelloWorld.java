f7ed6b0559f232e4a907bfc142634fe78eb507aa
Renato <renatovanderlei>
2021-12-10 11:50:54 -0300

    Segundo commit da prática 02

--- pratica_02/src/pratica_02/HelloWorld.java
+++ pratica_02/src/pratica_02/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	System.out.println("HelloWorld da prática 02");
-
+	System.out.println("Segundo commit da prática 02");
 	}
 
 }
