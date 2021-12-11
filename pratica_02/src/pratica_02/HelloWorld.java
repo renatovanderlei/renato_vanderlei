82ef40c949ea3725510b41fc646e5a8861e432b5
Renato Vanderlei <nato_vanderlei@hotmail.com>
2021-12-11 14:13:48 -0300

    Commit na main pós commit da HOTFIX

--- pratica_02/src/pratica_02/HelloWorld.java
+++ pratica_02/src/pratica_02/HelloWorld.java
@@ -3,8 +3,8 @@ package pratica_02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld pratica_02");//
-
+		System.out.println("HelloWorld pratica_02");
+		System.out.println("HelloWorld do COMMIT NA MAIN APÓS O PRIMEIRO DA HOTFIX");
 	}
 
 }
