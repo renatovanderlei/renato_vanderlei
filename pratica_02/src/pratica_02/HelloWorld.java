3ba58ecbf23659c29f417aa8257c40829ff3db71
Renato Vanderlei <nato_vanderlei@hotmail.com>
2021-12-11 14:12:34 -0300

    Criei a branch Hotfix, dei chekout nela e dei o primeiro commit

--- pratica_02/src/pratica_02/HelloWorld.java
+++ pratica_02/src/pratica_02/HelloWorld.java
@@ -3,7 +3,8 @@ package pratica_02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld pratica_02");//
+		System.out.println("HelloWorld pratica_02");
+		System.out.println("HelloWorld NA BRANCH HOTFIX");// primeiro commit na branch nova
 
 	}
 
