a15ccf776d5109a4ad1fc7eb3d757d1c0d143f67
Renato Vanderlei <nato_vanderlei@hotmail.com>
2021-12-16 23:18:23 -0300

    testando branching

--- pratica_02/src/pratica_02/HelloWorld.java
+++ pratica_02/src/pratica_02/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld pratica_02");
 		System.out.println("HelloWorld do MERGE entre Main e Hotfix");
-		System.out.println("Mais um teste de branching");
+		System.out.println("Mais um teste de branching");//testando
 
 	}
 
