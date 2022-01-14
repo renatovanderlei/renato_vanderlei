ca4c6897f26bd7ee05c55ef0fc0bd9e9ecd978d2
Renato Vanderlei <nato_vanderlei@hotmail.com>
2022-01-13 22:56:18 -0300

    Commit na hotfix

--- pratica_02/src/pratica_02/HelloWorld.java
+++ pratica_02/src/pratica_02/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("HelloWorld pratica_02");
 		System.out.println("HelloWorld NA BRANCH HOTFIX");// primeiro commit na branch nova
 		System.out.println("Agora uns commits na hotfix. A Main tá rolando tb mudanças!");
+		
+		// commit na hotfix
 	}
 
 }
