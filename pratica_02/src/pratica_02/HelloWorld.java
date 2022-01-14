eafd5ba0909bdf061600be02789480972b6ea120
Renato Vanderlei <nato_vanderlei@hotmail.com>
2022-01-13 23:02:16 -0300

    Novo commit depois do merge

--- pratica_02/src/pratica_02/HelloWorld.java
+++ pratica_02/src/pratica_02/HelloWorld.java
@@ -4,20 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld pratica_02");
-<<<<<<< HEAD
-		System.out.println("HelloWorld NA BRANCH HOTFIX");// primeiro commit na branch nova
-		System.out.println("Agora uns commits na hotfix. A Main tá rolando tb mudanças!");
-		
-		// commit na hotfix
-		System.out.println("Commit 2 na hotfix");
-	}
-=======
-		System.out.println("HelloWorld do MERGE entre Main e Hotfix");
-		System.out.println("Mais um teste de branching");//testando. Serasse vai?
->>>>>>> refs/heads/main
-
-		System.out.println("Merge da hotfix na main");
 		// Refazendo o branch
-		System.out.println("Mais um commit na main");
+		System.out.println("Merge novo da main e hotfix");
 	}
 }
